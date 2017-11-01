803aa2055fc9183566774a1e5963370e71a625b7
atpdev14 <tproctor4414@hotmail.com>
2017-11-01 15:28:56 -0700

    Song added.  More styling done.  Still needs actual questions.

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 var timer = 90;
 var intervalId;
+var onMyRadio = document.getElementById(onMyRadio);
+
 
 function run() {
 	intervalId = setInterval(decrement, 1000);	
@@ -11,7 +13,6 @@ function run() {
       if (timer === 0) {
         alert("Time Up!");
         checkAnswer();
-        timer = 90;
       }
     }
 
@@ -21,6 +22,13 @@ function run() {
      var question1 = document.quiz.question1.value;
      var question2 = document.quiz.question2.value;
      var question3 = document.quiz.question3.value;
+     var question4 = document.quiz.question4.value;
+     var question5 = document.quiz.question5.value;
+     var question6 = document.quiz.question6.value;
+     var question7 = document.quiz.question7.value;
+     var question8 = document.quiz.question8.value;
+     var question9 = document.quiz.question9.value;
+     var question10 = document.quiz.question10.value;
      var correctAnswers = 0;
 
      if (question1 === "Correct"){correctAnswers++};
@@ -29,8 +37,24 @@ function run() {
 
      if (question3 === "Correct"){correctAnswers++};
 
- 	$("#results").html("You got " + correctAnswers + " correct!");
+     if (question4 === "Correct"){correctAnswers++};
 
+     if (question5 === "Correct"){correctAnswers++};
+
+     if (question6 === "Correct"){correctAnswers++};
+
+     if (question7 === "Correct"){correctAnswers++};
+
+     if (question8 === "Correct"){correctAnswers++};
+
+     if (question9 === "Correct"){correctAnswers++};
+
+     if (question10 === "Correct"){correctAnswers++};
+
+
+ 	$("#results").html("You got " + correctAnswers + " correct!");
+ 	timer = 90;
     }
 
     run();
+    onMyRadio.play();
